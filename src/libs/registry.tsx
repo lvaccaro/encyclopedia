549d36f634f91d15f91415f26303f519020d2334
Luca Vaccaro <me@lvaccaro.com>
2025-05-27 17:50:58 +0200

    remove local metadatas
    Signed-off-by: Luca Vaccaro <me@lvaccaro.com>

--- src/libs/registry.tsx
+++ src/libs/registry.tsx
@@ -3,10 +3,6 @@
 export const policyAsset = '6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d';
 export const tether = 'ce091c998b83c78bb71a632313ba3760f1763d9cfcffae02258ffa9865a37bd2';
 
-export class Metadata {
-  stablecoin: boolean = false;
-  weight: number = 0;
-}
 export class Asset {
   id!: string;
   domain: string | undefined;
@@ -14,13 +10,10 @@ export class Asset {
   name: string | undefined;
   precision: number | undefined;
   icon: string | undefined;
-  metadata: Metadata | undefined;
 }
 export async function fetchAssets(): Promise<Map<string, Asset>> {
   const res = await fetch("/assets/assets.minimal.json")
   const text = await res.json();
-  const res_metadata = await fetch("/assets/assets_metadatas.json")
-  const metadatas: Map<string,(Metadata|null)> = await res_metadata.json() as Map<string,(Metadata|null)>;
   const assets: Map<string,(string|number|null)[]> = text as unknown as Map<string,(string|number|null)[]>;
   const list = new Map<string, Asset>();
   for (const a of Object.entries(assets)) {
@@ -31,12 +24,11 @@ export async function fetchAssets(): Promise<Map<string, Asset>> {
     asset.ticker = id == policyAsset ? 'LBTC' : a[1][1] as string || undefined
     asset.name = a[1][2] as string || undefined
     asset.precision = a[1][3] as number || undefined
-    for (const m of Object.entries(metadatas)) {
-      if (m[0] == a[0]) {
-        asset.metadata = m[1] as Metadata
-      }
-    }
     list.set(id, asset)
   }
   return list;
+}
+
+export async function fetchPriorityAssets(): Promise<string[]> {
+  const res = await fetch('/assets/priority_assets.json')
 }
